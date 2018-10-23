2ed5d0a599e731321755b51f736f81858aac7271
hedethie <43963704+hedethie@users.noreply.github.com>
2018-10-23 12:42:55 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
+'use strict';
 
 /*
- * GET home page.
+ get data from the service
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+const request = require('request');
+
+//TODO: Exercise 2 - Task 6 - Step 4
+
+const contentApiUrl = "http://localhost:3001";
+//const contentApiUrl = process.env.CONTENT_API_URL;
+
+function getSessions(cb) {
+    request(contentApiUrl + '/sessions', function(err, response, body) {
+        if (err) {
+            return cb(err);
+        }
+        const data = JSON.parse(body); // FRAGILE: ASSUME: valid JSON
+        cb(null, data);
+    });
+}
+
+function getSpeakers(cb) {
+    request(contentApiUrl + '/speakers', function(err, response, body) {
+        if (err) {
+            return cb(err);
+        }
+        const data = JSON.parse(body); // FRAGILE: ASSUME: valid JSON
+        cb(null, data);
+    });
+}
+
+function stats(cb) {
+    request(contentApiUrl + '/stats', function(err, response, body) {
+        if (err) {
+            return cb(err);
+        }
+        const data = JSON.parse(body);
+        cb(null, data);
+    });
+}
+
+module.exports = {
+    getSessions: getSessions,
+    getSpeakers: getSpeakers,
+    stats: stats
 };
